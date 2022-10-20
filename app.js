3fa9fe17a7bb965739e15d3739d72671121d35f7
watanabe-kf1983 <115921827+watanabe-kf1983@users.noreply.github.com>
2022-10-20 11:20:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -23,5 +23,6 @@ app.use(express.static(__dirname + '/static'));
 
 app.use('/', router);
 
-const port = process.env.PORT || 3000;
+const port = 3000;
+// const port = process.env.PORT || 3000;
 app.listen(port, () => { console.log(`Listening on port ${port}...`) });
